5743781df3fd222ad1f54698e409b7e5d6812ce4
Renan Andrade <eu@renanandrade.com.br>
2020-06-05 22:00:16 -0300

    calculate a pizza dough

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
 
-function App() {
+export default function App() {
+  const [ingredients, setIngredients] = useState({
+    flour: 400,
+    water: 0,
+    salt: 0,
+    yeast: 0,
+  });
+
+  const calculateIngredients = useCallback(() => {
+    const water = Math.floor(ingredients.flour * 0.65);
+    const salt = Math.floor(ingredients.flour * 0.04);
+    const yeast = Math.floor(ingredients.flour * 0.01);
+
+    setIngredients((old) => ({ ...old, water, salt, yeast }));
+  }, [ingredients.flour]);
+
+  useEffect(() => {
+    calculateIngredients();
+  }, [calculateIngredients]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Calculadora Massa de Pizza</h1>
+
+      <h2>Quantidade de Farinha de Trigo</h2>
+      <input
+        type="number"
+        step="10"
+        min="10"
+        value={ingredients.flour}
+        onChange={(event) =>
+          setIngredients({ ...ingredients, flour: event.target.value })
+        }
+      />
+
+      <h3>Receita</h3>
+      <ul>
+        <li>Farinha: {ingredients.flour}g</li>
+        <li>Água: {ingredients.water}ml</li>
+        <li>Sal: {ingredients.salt}g</li>
+        <li>Fermento: {ingredients.yeast}g</li>
+      </ul>
     </div>
   );
 }
-
-export default App;
